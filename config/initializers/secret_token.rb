d2905524b4caeb26af21131c470582d485a8442a
Mark Bennett <mark@burmis.ca>
2012-02-24 19:55:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dayhomes::Application.config.secret_token = '228ab36d7dd98b4a0b952cb106d0a0c1b1b265659cb848a72ae59528fc10209061e2f93f6816804d985886db88f2be02a234e71d70b3f22e0e99234a639ec573'
