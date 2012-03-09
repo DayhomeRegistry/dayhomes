1cca7b1ecd53a799fcdbefcba1dfedf18c637558
Ryan Jones <ryan.michael.jones@gmail.com>
2012-03-08 20:54:23 -0700

    merged in master

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120306201536) do
+ActiveRecord::Schema.define(:version => 20120307173951) do
 
   create_table "day_homes", :force => true do |t|
     t.string   "name"
     t.float    "lat"
     t.float    "lng"
-    t.string   "address"
     t.boolean  "gmaps"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
     t.string   "city"
     t.string   "province"
     t.string   "street1"
     t.string   "street2"
     t.string   "postal_code"
+    t.integer  "enrolled",       :default => 0,  :null => false
+    t.integer  "max_enrollment", :default => 10, :null => false
   end
 
 end
