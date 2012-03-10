ce398b532e8ebdc87f2f288c57a45b2eb3e0b26e
Ryan Jones <ryan.michael.jones@gmail.com>
2012-03-10 12:02:48 -0700

    add some logic to only process advanced when it's an advanced form.

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,36 +3,33 @@ class Search
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :address, :availability_types
+  attr_accessor :address, :availability_types, :advanced_search
 
   def initialize(attributes = {})
-    # set the availability types
-    self.availability_types = AvailabilityType.order('kind asc').all
-
-    # "set" each of the attributes
+    # set each of the attributes
     attributes.each do |name, value|
-
-      # don't update the availabilty_type checkboxes to an array
-      unless name == 'availability_types'
         send("#{name}=", value)
-      end
     end
 
-    ##TODO maybe a check for only advanced search page?
+    # override the availability types array (checkboxes)) with the actual AvailabilityType models
+    self.availability_types = AvailabilityType.order('kind asc').all
 
-    # save the checked items in the model
-    if attributes.has_key?(:availability_types)
-      attributes[:availability_types][:kind].each do |parm_avail_type|
-        self.availability_types.each do |avail_type|
+    # only get the availability type processing if you're on the advanced form
+    if self.advanced_search == 'true'
+      # save the checked items in the model
+      if attributes.has_key?(:availability_types)
+        attributes[:availability_types][:kind].each do |parm_avail_type|
+          self.availability_types.each do |avail_type|
 
-          # Set the checkboxes if the user has a checkbox checked
-          if (avail_type.id.to_s == parm_avail_type)
-            avail_type.checked = true
+            # Set the checkboxes if the user has a checkbox checked
+            if (avail_type.id.to_s == parm_avail_type)
+              avail_type.checked = true
+            end
           end
         end
       end
-    end
 
+    end
   end
 
   def persisted?
