132e196808a9364e0fdbcf946ef44b1c992bf0a9
Ryan Jones <ryan.michael.jones@gmail.com>
2012-03-17 11:36:13 -0600

    Added index migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120314200758) do
+ActiveRecord::Schema.define(:version => 20120317162332) do
 
   create_table "availability_types", :force => true do |t|
     t.string   "kind"
@@ -32,12 +32,18 @@ ActiveRecord::Schema.define(:version => 20120314200758) do
     t.datetime "updated_at",           :null => false
   end
 
+  add_index "day_home_availability_types", ["availability_type_id"], :name => "index_day_home_availability_types_on_availability_type_id"
+  add_index "day_home_availability_types", ["day_home_id"], :name => "index_day_home_availability_types_on_day_home_id"
+
   create_table "day_home_certification_types", :force => true do |t|
     t.integer  "day_home_id"
     t.integer  "certification_type_id"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
   end
+
+  add_index "day_home_certification_types", ["certification_type_id"], :name => "index_day_home_certification_types_on_certification_type_id"
+  add_index "day_home_certification_types", ["day_home_id"], :name => "index_day_home_certification_types_on_day_home_id"
 
   create_table "day_homes", :force => true do |t|
     t.string   "name"
