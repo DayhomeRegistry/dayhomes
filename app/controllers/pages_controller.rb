051b2e461cffe23d03100d5f79644a8d170b7cbb
Nathan Bertram <nbertram@gmail.com>
2012-04-24 20:30:03 -0600

    Reject featured day_homes without a photo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @featured_day_homes = DayHome.featured
+    @featured_day_homes = DayHome.featured.reject{|day_home| day_home.photos.blank? }
   end
   
   def about
