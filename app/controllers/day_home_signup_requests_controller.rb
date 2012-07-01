905cdcd43f8c7fc75023831b6033e0af63540850
Jon Holt <jonmholt@gmail.com>
2012-06-30 20:21:49 -0600

    temp fix for form_builder and update to pricing

--- app/controllers/day_home_signup_requests_controller.rb
+++ app/controllers/day_home_signup_requests_controller.rb
@@ -4,11 +4,18 @@ class DayHomeSignupRequestsController < ApplicationController
   end
   
   def create
+    # Capture the signup request and send email
     @day_home_signup_request = DayHomeSignupRequest.new(params[:day_home_signup_request])
+    
+    # Create a dayhome
     @day_home = DayHome.create_from_signup(@day_home_signup_request)
     @day_home.update_attributes(params[:day_home])
-	#@day_home.licensed=params['day_home[licensed]']
-	#@day_home.licensed=params['dayhome[dietary_accommodations]']
+    
+    # Create user, if necessary
+    
+    # Bind dayhome to user
+    
+    
 	if @day_home.save
 	  if @day_home_signup_request.save
         redirect_to root_path, :notice => "Thanks for adding your DayHome! We will contact you soon!"
