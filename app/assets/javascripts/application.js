1ac26b0c7f8bb1905c49b47fd18751b8547492e3
Jon Holt <jonmholt@gmail.com>
2012-09-04 16:26:02 -0600

    validation?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// taking this out
-// require rails.validations
 
 //= require jquery
 //= require jquery_ujs
+//= require rails.validations
 //= require bootstrap
 //= require fullcalendar
 //= require_tree ./gmaps4rails
