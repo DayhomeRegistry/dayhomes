7c85d1519ba158a5ddedb62ec1225354548442ff
Jon Holt <jonmholt@gmail.com>
2012-09-15 21:06:17 -0600

    garsh

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
 set :rails_env, "staging"
+set :bundle_flags, "--deployment --binstubs --without development test darwin sunos mswin"
 
 server '198.101.207.75', :app, :web, :db, :primary => true
--- start-web.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Set the environment
-call C:\_LOCALdata\Ruby193\bin\setrbvars.bat
-
-REM Run the web server
-cd C:\_LOCALdata\Projects\dayhomes
-rails server
