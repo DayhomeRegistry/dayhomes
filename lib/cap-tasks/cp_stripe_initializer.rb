5e9162ce8ca173ce4b471d0f7055281f49e477bc
Jon Holt <jonmholt@gmail.com>
2013-03-13 12:43:50 -0600

    sigh

--- lib/cap-tasks/cp_stripe_initializer.rb
+++ lib/cap-tasks/cp_stripe_initializer.rb
@@ -1,7 +1,7 @@
 namespace :deploy do
   desc "cp_stripe_initializer"
   task :cp_stripe_initializer do
-    run "cp /srv/dayhomes_staging/shared/config/initializers/stripe.rb /srv/dayhomes_staging/current/config/initializers/stripe.rb"
+    run "cp /srv/dayhomes_production/shared/config/initializers/stripe.rb /srv/dayhomes_production/current/config/initializers/stripe.rb"
 
   end
 end
