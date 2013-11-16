7bc52e45a530d341e5ae2c8de5f80db27aab827a
Jon Holt <jonmholt@gmail.com>
2013-11-16 09:39:14 -0700

    gotta keep the old plan so that they can upgrade from it

--- db/migrate/20131113043232_add_annual_plan.rb
+++ db/migrate/20131113043232_add_annual_plan.rb
@@ -1,15 +1,24 @@
 class AddAnnualPlan < ActiveRecord::Migration
   def up
   	plan=Plan.find_by_name("Baby Bear")
-  	plan.plan="babyannual"
-  	plan.price=21
+  	plan.inactive=DateTime.now()
   	plan.save
+    plan = Plan.new()
+    plan.plan="babyannual"
+    plan.price="21"
+    plan.name="Baby Bear(a)"
+    plan.day_homes=1
+    plan.staff=0
+    plan.locales=1
+    plan.block_staff_addon=1
+    plan.block_locales_addon=1
+    plan.active = DateTime.now()
+    plan.save
   end
 
   def down
-  	plan=Plan.find_by_name("Baby Bear")
-  	plan.plan="baby"
-  	plan.price=0
-  	plan.save
+  	plan=Plan.find_by_plan("babyannual")
+  	plan.destroy
+
   end
 end
--- db/migrate/20131114032934_add_events_to_plan.rb
+++ db/migrate/20131114032934_add_events_to_plan.rb
@@ -2,7 +2,7 @@ class AddEventsToPlan < ActiveRecord::Migration
   def up
   	add_column :plans, :subscription, :string, :default=>"mth"
   	add_column :plans, :events, :integer,:default=>0
-  	plan=Plan.find_by_name("Baby Bear")
+  	plan=Plan.where("inactive is null").find_by_plan("babyannual")
   	plan.subscription="yr"
   	plan.events=1
     plan.free_features=1
