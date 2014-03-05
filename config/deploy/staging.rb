a70cbc5295c31db3d78918932634c9f407392cba
Jon Holt <jonmholt@gmail.com>
2014-03-04 23:05:26 -0700

    update staging ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :rails_env, "staging"
 set :bundle_flags, "--deployment --binstubs --without development test darwin sunos mswin"
 
-server '198.101.207.75', :app, :web, :db, :primary => true
+server '166.78.149.139', :app, :web, :db, :primary => true
