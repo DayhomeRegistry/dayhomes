ee537a76e18c6e1ca1ad69920f7e3b5e6d140a53
Jon Holt <jonmholt@gmail.com>
2014-06-13 17:48:22 -0600

    update the analytics code and update the adsense yet again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Dayhomes::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(jquery.home_slider.js google_analytics.js upgrade.js upgrade_init.js billing.js billing_init.js organization.js categories.css mailer.css pages.css)
+  config.assets.precompile += %w(jquery.home_slider.js upgrade.js upgrade_init.js billing.js billing_init.js organization.js categories.css mailer.css pages.css)
     
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
