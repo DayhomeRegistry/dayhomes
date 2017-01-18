b48d83122e8c0bd06b842dc25cd14620944c7596
Jon Holt <jonmholt@gmail.com>
2017-01-18 13:02:53 -0700

    cp_gmaps

---
+++ lib/cap-tasks/cp_gmaps_settings.rb
@@ -0,0 +1,10 @@
+namespace :deploy do
+	desc <<-DESC
+	  [internal] Copies gmaps.yml from shared_path into release_path.
+	DESC
+	task :cp_facebook_settings do
+		run "cp #{shared_path}/config/gmaps.yml #{release_path}/config/gmaps.yml"
+	end
+end
+
+after "deploy:finalize_update", 'deploy:cp_gmaps_settings'
