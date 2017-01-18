9c4db9ff4782e23237c7235fd717384e501938fd
Jon Holt <jonmholt@gmail.com>
2017-01-17 19:04:23 -0700

    gmaps api keys

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,2 +1,4 @@
 APPLICATION_CONFIG = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env].with_indifferent_access
+GMAPS_CONFIG = YAML.load_file("#{Rails.root}/config/gmaps.yml")[Rails.env].with_indifferent_access
+
 
